a26b5f7455d17f4a90cd1df13e581ba9fe0fb8d2
Shreyasi2002 <shreyasi18sia@gmail.com>
2021-09-16 21:03:46 +0530

    Arrow and Default Functions

--- Modern JavaScript/functions.js
+++ Modern JavaScript/functions.js
@@ -12,7 +12,7 @@ The above code will not give any errors.
 
 // Function Expression
 
-funcExpression();
+// funcExpression();
 
 const funcExpression = function () {
   const t = 5;
@@ -46,4 +46,54 @@ In JavaScript, "Hoisting" is the default behavior of moving all the declarations
 at the top of the scope before code execution. Basically, it gives us an advantage 
 that no matter where functions and variables are declared, they are moved to the 
 top of their scope regardless of whether their scope is global or local.
+*/
+
+// Default Function
+
+function newFunc(firstArg, secondArg = "Hello") {
+  console.log(firstArg, secondArg);
+}
+
+/*
+Note that secondArg is a default argument.
+*/
+
+newFunc(11, undefined);
+
+/*
+We get the output -
+11 Hello
+
+The 'undefined' keyword means not specified. Line 61 is same as -
+  newFunc(11);
+
+In these situations, the default argument is used. ("Hello" is assigned to secondArg)
+*/
+
+newFunc("Shreyasi", "Mandal");
+
+/*
+The above gives the output -
+Shreyasi Mandal
+
+This is because both the arguments are specified here.
+*/
+
+// Arrow Functions () => {}
+
+/*
+Remember, arrow functions can only be used as function expressions.
+The difference between arrow and default functions lies mainly with the usage of 
+'this' keyword.
+*/
+
+const arrowFunc = (firstArg = "Hello", secondArg = "^_^") => {
+  console.log(firstArg, secondArg);
+};
+
+arrowFunc();
+
+/*
+We get the output -
+Hello ^_^
 */
