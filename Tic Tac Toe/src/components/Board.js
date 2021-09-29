34c788c3b8c7fea14575339014e6ea5b6612e857
Shreyasi2002 <shreyasi18sia@gmail.com>
2021-09-29 09:40:24 +0530

    Added components Board.js and Square.js

---
+++ Tic Tac Toe/src/components/Board.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Square from './Square';
+
+const Board = () => {
+  return (
+    <div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+      <div>
+        <Square />
+        <Square />
+        <Square />
+      </div>
+    </div>
+  );
+};
+
+export default Board;
---
+++ Tic Tac Toe/src/components/Square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square = () => {
+  return <button type="button">1</button>;
+};
+
+export default Square;
